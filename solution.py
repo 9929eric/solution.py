09d4d1103183857e65b8f9fa5b9dc56d034688e1
9929eric <43610219+9929eric@users.noreply.github.com>
2018-09-26 09:35:47 -0400

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,45 @@
+beeps = 2
+boops = 6
+tot1 = beeps + boops
+print(tot1)
+beeps = 0
+boops = 5
+tot2 = beeps + boops
+print(tot2)
+beeps = 9
+boops = 3
+tot3 = beeps + boops
+print(tot3)
+beeps = 4
+boops = 8
+tot4 = beeps + boops
+print(tot4)
+beeps = 10
+boops = 5
+tot5 = beeps + boops
+print(tot5)
+beeps = 0
+boops = 0
+tot6 = beeps + boops
+print(tot6)
+beeps = 11
+boops = 12
+tot7 = beeps + boops
+print(tot7)
+beeps = 5
+boops = 5
+tot8 = beeps + boops
+print(tot8)
+beeps = 1
+boops = 17
+tot9 = beeps + boops
+print(tot9)
+beeps = 5
+boops = 7
+tot10 = beeps + boops
+print(tot10)
+beeps = 4
+boops = 0
+tot11 = beeps + boops
+print(tot11)
+print("HELLO WJRLD")
